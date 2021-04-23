d5a2df644e68a817e51e65a33678b26149d9ded4
Pavel Kochenda <peeoner174@gmail.com>
2021-04-24 00:33:20 +0500

    [add] dispatch handler async opportunity

--- Sources/UseCase-Combine/Dispatcher.swift
+++ Sources/UseCase-Combine/Dispatcher.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 public class Dispatcher<CommandType: Command> {
     public typealias Handler = (CommandType) -> Void
@@ -19,9 +20,21 @@ public class Dispatcher<CommandType: Command> {
 
     /// this method dispatch command to Handler
     /// - Parameter command: A Command that request process to handler
-    public func dispatch(_ command: CommandType) {
-        queue.async {
-            self.handler(command)
+    public func dispatch(
+        _ command: CommandType,
+        async: Bool = false
+    ) {
+        switch async {
+        case true:
+            queue.async { [weak self] in
+                guard let self = self else { return }
+                self.handler(command)
+            }
+        case false:
+            queue.sync { [weak self] in
+                guard let self = self else { return }
+                self.handler(command)
+            }
         }
     }
 }
--- Sources/UseCase-Combine/UseCase.swift
+++ Sources/UseCase-Combine/UseCase.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Combine
 
 public protocol Command {
     associatedtype State: Equatable
